b3e8f7afd7c61efee13a2af77dc84fccb027deb8
Plastix <aidanpieper@gmail.com>
2016-10-07 18:49:12 -0400

    Fix RecyclerView IllegalStateException: Closes #21

--- example/src/main/java/com/genius/groupie/example/InfiniteScrollListener.java
+++ example/src/main/java/com/genius/groupie/example/InfiniteScrollListener.java
@@ -1,5 +1,6 @@
 package com.genius.groupie.example;
 
+import android.os.Handler;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
@@ -11,9 +12,11 @@ public abstract class InfiniteScrollListener extends RecyclerView.OnScrollListen
     private int firstVisibleItem, visibleItemCount, totalItemCount;
     private int current_page = 0;
     private LinearLayoutManager linearLayoutManager;
+    private Handler handler;
 
     public InfiniteScrollListener(LinearLayoutManager linearLayoutManager) {
         this.linearLayoutManager = linearLayoutManager;
+        this.handler = new Handler();
     }
 
     public void setLinearLayoutManager(LinearLayoutManager linearLayoutManager) {
@@ -21,26 +24,36 @@ public abstract class InfiniteScrollListener extends RecyclerView.OnScrollListen
     }
 
     @Override
-    public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+    public void onScrolled(final RecyclerView recyclerView, int dx, int dy) {
         super.onScrolled(recyclerView, dx, dy);
 
-        visibleItemCount = recyclerView.getChildCount();
-        totalItemCount = linearLayoutManager.getItemCount();
-        firstVisibleItem = linearLayoutManager.findFirstVisibleItemPosition();
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                visibleItemCount = recyclerView.getChildCount();
+                totalItemCount = linearLayoutManager.getItemCount();
+                firstVisibleItem = linearLayoutManager.findFirstVisibleItemPosition();
 
-        if (loading) {
-            if (totalItemCount > previousTotal || totalItemCount == 0) {
-                loading = false;
-                previousTotal = totalItemCount;
+                if (loading) {
+                    if (totalItemCount > previousTotal || totalItemCount == 0) {
+                        loading = false;
+                        previousTotal = totalItemCount;
+                    }
+                }
+
+                // End has been reached
+                if (!loading && (totalItemCount - visibleItemCount) <= (firstVisibleItem + visibleThreshold)) {
+                    current_page++;
+                    onLoadMore(current_page);
+                    loading = true;
+                }
             }
-        }
+        };
 
-        // End has been reached
-        if (!loading && (totalItemCount - visibleItemCount) <= (firstVisibleItem + visibleThreshold)) {
-            current_page++;
-            onLoadMore(current_page);
-            loading = true;
-        }
+        // We shouldn't directly mutate the RecyclerView data in onScrolled
+        // This is because it may be called during the measure & layout pass where you cannot change the data
+        // Post-pone any adapter changes until the next frame
+        handler.post(runnable);
     }
 
     public abstract void onLoadMore(int current_page);
