01058d49397fd426cfa7642733e3c316f68e3af2
Lisa Wray <lisa@genius.com>
2016-10-13 00:12:26 -0400

    Cleanup. Fix warning about modifying adapter in scroll state.

--- example/src/main/java/com/genius/groupie/example/InfiniteScrollListener.java
+++ example/src/main/java/com/genius/groupie/example/InfiniteScrollListener.java
@@ -1,6 +1,5 @@
 package com.genius.groupie.example;
 
-import android.os.Handler;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
@@ -10,13 +9,17 @@ public abstract class InfiniteScrollListener extends RecyclerView.OnScrollListen
     private boolean loading = true; // True if we are still waiting for the last set of data to load.
     private final int visibleThreshold = 5; // The minimum amount of items to have below your current scroll position before loading more.
     private int firstVisibleItem, visibleItemCount, totalItemCount;
-    private int current_page = 0;
+    private int currentPage = 0;
     private LinearLayoutManager linearLayoutManager;
-    private Handler handler;
+    private Runnable loadMore = new Runnable() {
+        @Override
+        public void run() {
+            onLoadMore(currentPage);
+        }
+    }
 
     public InfiniteScrollListener(LinearLayoutManager linearLayoutManager) {
         this.linearLayoutManager = linearLayoutManager;
-        this.handler = new Handler();
     }
 
     public void setLinearLayoutManager(LinearLayoutManager linearLayoutManager) {
@@ -26,34 +29,23 @@ public abstract class InfiniteScrollListener extends RecyclerView.OnScrollListen
     @Override
     public void onScrolled(final RecyclerView recyclerView, int dx, int dy) {
         super.onScrolled(recyclerView, dx, dy);
+        visibleItemCount = recyclerView.getChildCount();
+        totalItemCount = linearLayoutManager.getItemCount();
+        firstVisibleItem = linearLayoutManager.findFirstVisibleItemPosition();
 
-        final Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                visibleItemCount = recyclerView.getChildCount();
-                totalItemCount = linearLayoutManager.getItemCount();
-                firstVisibleItem = linearLayoutManager.findFirstVisibleItemPosition();
-
-                if (loading) {
-                    if (totalItemCount > previousTotal || totalItemCount == 0) {
-                        loading = false;
-                        previousTotal = totalItemCount;
-                    }
-                }
-
-                // End has been reached
-                if (!loading && (totalItemCount - visibleItemCount) <= (firstVisibleItem + visibleThreshold)) {
-                    current_page++;
-                    onLoadMore(current_page);
-                    loading = true;
-                }
+        if (loading) {
+            if (totalItemCount > previousTotal || totalItemCount == 0) {
+                loading = false;
+                previousTotal = totalItemCount;
             }
-        };
+        }
 
-        // We shouldn't directly mutate the RecyclerView data in onScrolled
-        // This is because it may be called during the measure & layout pass where you cannot change the data
-        // Post-pone any adapter changes until the next frame
-        handler.post(runnable);
+        // End has been reached
+        if (!loading && (totalItemCount - visibleItemCount) <= (firstVisibleItem + visibleThreshold)) {
+            currentPage++;
+            recyclerView.post(loadMore);
+            loading = true;
+        }
     }
 
     public abstract void onLoadMore(int current_page);
