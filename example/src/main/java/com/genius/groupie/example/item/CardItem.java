d1c0241686e850239bed72c973803394b35e30cd
Lisa Wray <lisa@genius.com>
2016-10-20 17:05:06 -0400

    Change View.OnClickListener to OnItemClickListener
    This prevents the necessity of asking the adapter for the clicked
    item. It's especially convenient if you have multiple nested adapters.
    
    Also consolidate the onBind logic in GroupAdapter to clean up uncalled
    code.

--- example/src/main/java/com/genius/groupie/example/item/CardItem.java
+++ example/src/main/java/com/genius/groupie/example/item/CardItem.java
@@ -3,13 +3,13 @@ package com.genius.groupie.example.item;
 import android.support.annotation.ColorRes;
 
 import com.genius.groupie.Item;
-import com.genius.groupie.example.MainActivity;
 import com.genius.groupie.example.databinding.ItemCardBinding;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.genius.groupie.example.MainActivity.*;
+import static com.genius.groupie.example.MainActivity.INSET;
+import static com.genius.groupie.example.MainActivity.INSET_TYPE_KEY;
 
 public class CardItem extends Item<ItemCardBinding> {
 
@@ -42,5 +42,9 @@ public class CardItem extends Item<ItemCardBinding> {
         Map<String, Object> map = new HashMap<>();
         map.put(INSET_TYPE_KEY, INSET);
         return map;
+    }
+
+    public CharSequence getText() {
+        return text;
     }
 }
