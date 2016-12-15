2eebe0a229bcc052656038922c56820e01d1aeca
Ken Jones <kjones@peaksware.com>
2016-12-15 07:32:52 -0700

    Use wildcard parameterized types for lists of Groups passed to the API. Allows library consumers to maintain lists of objects that implement Group. i.e. List<MyGroup> instead of List<Group>

--- groupie/src/main/java/com/genius/groupie/NestedGroup.java
+++ groupie/src/main/java/com/genius/groupie/NestedGroup.java
@@ -23,7 +23,7 @@ public abstract class NestedGroup implements Group, GroupDataObserver {
         return size;
     }
 
-    protected final int getItemCount(List<Group> groups) {
+    protected final int getItemCount(List<? extends Group> groups) {
         int size = 0;
         for (Group group : groups) {
             size += group.getItemCount();
@@ -84,7 +84,7 @@ public abstract class NestedGroup implements Group, GroupDataObserver {
     }
 
     @CallSuper
-    public void addAll(List<Group> groups) {
+    public void addAll(List<? extends Group> groups) {
         for (Group group : groups) {
             group.setGroupDataObserver(this);
         }
@@ -96,7 +96,7 @@ public abstract class NestedGroup implements Group, GroupDataObserver {
     }
 
     @CallSuper
-    public void addAll(int position, List<Group> groups) {
+    public void addAll(int position, List<? extends Group> groups) {
         for (Group group : groups) {
             group.setGroupDataObserver(this);
         }
