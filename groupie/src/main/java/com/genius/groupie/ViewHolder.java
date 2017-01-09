71b02c5e7b3746fd4fb32e7384cc7ab64a118c13
Lisa Wray <lisa@genius.com>
2017-01-09 13:05:51 -0500

    Move OnItemClickListener handling into ViewHolder. Add check for NO_POSITION. Fixes #42 (#66)

--- groupie/src/main/java/com/genius/groupie/ViewHolder.java
+++ groupie/src/main/java/com/genius/groupie/ViewHolder.java
@@ -2,24 +2,57 @@ package com.genius.groupie;
 
 import android.databinding.ViewDataBinding;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import java.util.Map;
+
+import static android.support.v7.widget.RecyclerView.NO_POSITION;
 
 public class ViewHolder<T extends ViewDataBinding> extends RecyclerView.ViewHolder {
     public final T binding;
     private Item item;
+    private OnItemClickListener onItemClickListener;
+
+    private View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // Discard click if the viewholder has been removed, but was still in the process of
+            // animating its removal while clicked (unlikely, but technically possible)
+            if (getAdapterPosition() != NO_POSITION) {
+                onItemClickListener.onItemClick(getItem(), v);
+            }
+        }
+    };
 
     public ViewHolder(T binding) {
         super(binding.getRoot());
         this.binding = binding;
     }
 
-    public void bind(Item item) {
+    public void bind(Item item, OnItemClickListener onItemClickListener) {
         this.item = item;
+
+        // Only set the top-level click listener if a) the adapter has one, and b) the item has
+        // click enabled.  This ensures we don't interfere with user-set click listeners.
+
+        // It would be nice to keep our listener always attached and set it only once on creating
+        // the viewholder, but different items of the same layout type may not have the same click
+        // listener or even agree on whether they are clickable.
+        if (onItemClickListener != null && item.isClickable()) {
+            binding.getRoot().setOnClickListener(onClickListener);
+        }
     }
 
     public void unbind() {
+        // Only set the top-level click listener to null if we had previously set it ourselves.
+
+        // This avoids undoing any click listeners the user may set which might be persistent for
+        // the life of the viewholder. (It's up to the user to make sure that's correct behavior.)
+        if (onItemClickListener != null && item.isClickable()) {
+            binding.getRoot().setOnClickListener(null);
+        }
         this.item = null;
+        this.onItemClickListener = null;
     }
 
     public Map<String, Object> getExtras() {
