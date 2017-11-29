7369a329a09e6080b7b6bde36436b87e4a68e8df
Simon Marquis <contact@simon-marquis.fr>
2017-11-29 15:52:08 +0100

    Fixes #118 full bleed item not being full bleed
    FullBleedCardItem already inherits the CardItem inset.
    Removing the extra parameter in init fixes the issue.

--- example/src/main/java/com/xwray/groupie/example/item/FullBleedCardItem.kt
+++ example/src/main/java/com/xwray/groupie/example/item/FullBleedCardItem.kt
@@ -1,12 +1,11 @@
 package com.xwray.groupie.example.item
 
 import android.support.annotation.ColorRes
-import com.xwray.groupie.example.INSET
 import com.xwray.groupie.example.INSET_TYPE_KEY
 
 class FullBleedCardItem(@ColorRes colorRes: Int) : CardItem(colorRes) {
 
     init {
-        extras.put(INSET_TYPE_KEY, INSET)
+        extras.remove(INSET_TYPE_KEY)
     }
 }
