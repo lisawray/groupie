196dc2c790ae17abf0c840d74d813ff922b5eb6b
Lisa Wray <lisawray@gmail.com>
2017-12-27 20:16:15 -0500

    Update library versions (#138)

--- library-kotlin-android-extensions/src/main/java/com/xwray/groupie/kotlinandroidextensions/ViewHolder.kt
+++ library-kotlin-android-extensions/src/main/java/com/xwray/groupie/kotlinandroidextensions/ViewHolder.kt
@@ -1,9 +1,7 @@
 package com.xwray.groupie.kotlinandroidextensions
 
-import android.support.v7.widget.RecyclerView
 import android.view.View
 import kotlinx.android.extensions.LayoutContainer
 
 class ViewHolder(override val containerView: View) : com.xwray.groupie.ViewHolder(containerView),
-        LayoutContainer {
-}
+        LayoutContainer
