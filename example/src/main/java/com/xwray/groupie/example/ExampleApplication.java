b21c6be2032e16c3e9c092d46d0991f1f1634009
Fabio Bombardi <fabio.bombardi@mmbsoftware.it>
2018-02-20 11:14:20 +0100

    Add AppCompatDelegate.setCompatVectorFromResourcesEnabled(true); in order to full manage vecotor drawable for API level < 21: https://developer.android.com/reference/android/support/v7/app/AppCompatDelegate.html#setCompatVectorFromResourcesEnabled(boolean)

---
+++ example/src/main/java/com/xwray/groupie/example/ExampleApplication.java
@@ -0,0 +1,11 @@
+package com.xwray.groupie.example;
+
+import android.app.Application;
+import android.support.v7.app.AppCompatDelegate;
+
+
+public class ExampleApplication extends Application {
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
+}
