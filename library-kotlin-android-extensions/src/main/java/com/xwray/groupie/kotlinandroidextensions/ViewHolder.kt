3edac79593b5b32d91b350a335b5dc6c6aa35360
Valentin Hinov <val@rockspin.co.uk>
2019-08-22 13:50:09 +0100

    Issue #235 Fix Kotlin synthetic caching

--- library-kotlin-android-extensions/src/main/java/com/xwray/groupie/kotlinandroidextensions/ViewHolder.kt
+++ library-kotlin-android-extensions/src/main/java/com/xwray/groupie/kotlinandroidextensions/ViewHolder.kt
@@ -1,7 +1,10 @@
 package com.xwray.groupie.kotlinandroidextensions
 
 import android.view.View
+import kotlinx.android.extensions.CacheImplementation
+import kotlinx.android.extensions.ContainerOptions
 import kotlinx.android.extensions.LayoutContainer
 
+@ContainerOptions(cache = CacheImplementation.HASH_MAP)
 class ViewHolder(override val containerView: View) : com.xwray.groupie.ViewHolder(containerView),
         LayoutContainer
