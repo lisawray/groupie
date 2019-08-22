c6bbfe3d7ab6adfa5ac259f0ccb8c4bb543887f9
Valentin Hinov <val@rockspin.co.uk>
2019-08-22 13:56:17 +0100

    New release 2.5.1

--- library-kotlin-android-extensions/src/main/java/com/xwray/groupie/kotlinandroidextensions/ViewHolder.kt
+++ library-kotlin-android-extensions/src/main/java/com/xwray/groupie/kotlinandroidextensions/ViewHolder.kt
@@ -5,6 +5,8 @@ import kotlinx.android.extensions.CacheImplementation
 import kotlinx.android.extensions.ContainerOptions
 import kotlinx.android.extensions.LayoutContainer
 
+// Need to specify ContainerOptions in order for caching to work.
+// See: https://youtrack.jetbrains.com/oauth?state=%2Fissue%2FKT-28617
 @ContainerOptions(cache = CacheImplementation.HASH_MAP)
 class ViewHolder(override val containerView: View) : com.xwray.groupie.ViewHolder(containerView),
         LayoutContainer
