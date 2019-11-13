a46f2bcd91f8d43cc6f52c1c67200bad0818bbe6
NUmeroAndDev <numero.dev.and@gmail.com>
2019-11-13 10:21:10 +0900

    Fixed to to reset carousel position when CaroucelItem is displayed again

--- example/src/main/java/com/xwray/groupie/example/item/CarouselItem.kt
+++ example/src/main/java/com/xwray/groupie/example/item/CarouselItem.kt
@@ -1,5 +1,6 @@
 package com.xwray.groupie.example.item
 
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.xwray.groupie.GroupAdapter
@@ -7,6 +8,7 @@ import com.xwray.groupie.example.R
 import com.xwray.groupie.kotlinandroidextensions.Item
 import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
 import kotlinx.android.synthetic.main.item_carousel.*
+import kotlinx.android.synthetic.main.item_carousel.view.*
 
 /**
  * A horizontally scrolling RecyclerView, for use in a vertically scrolling RecyclerView.
@@ -18,11 +20,16 @@ class CarouselItem(private val carouselDecoration: RecyclerView.ItemDecoration,
         return R.layout.item_carousel
     }
 
-    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
-        viewHolder.recyclerView.apply {
+    override fun createViewHolder(itemView: View): GroupieViewHolder {
+        itemView.recyclerView.apply {
             layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
             adapter = carouselAdapter
+        }
+        return super.createViewHolder(itemView)
+    }
 
+    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+        viewHolder.recyclerView.apply {
             // We don't know if the layout we're passed has been bound before so
             // we need to ensure we don't register the item decoration multiple times,
             // by trying to remove it first. (Nothing happens if it's not registered.)
