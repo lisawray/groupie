d4312411fca447fe5dcc95ad2719d703e99dab7c
Thomas HERBETH <thomas.herbeth@gmail.com>
2020-07-04 23:12:17 +0200

    [+] Making Kotlin extensions work with all classes extending GroupieViewHolder.

--- library-kotlin-android-extensions/src/main/java/com/xwray/groupie/groupiex/GroupAdapterExt.kt
+++ library-kotlin-android-extensions/src/main/java/com/xwray/groupie/groupiex/GroupAdapterExt.kt
@@ -4,14 +4,14 @@ import com.xwray.groupie.Group
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 
-operator fun GroupAdapter<GroupieViewHolder>.plusAssign(element: Group) = this.add(element)
+operator fun GroupAdapter<out GroupieViewHolder>.plusAssign(element: Group) = this.add(element)
 
 
-operator fun GroupAdapter<GroupieViewHolder>.plusAssign(groups: Collection<Group>) = this.addAll(groups)
+operator fun GroupAdapter<out GroupieViewHolder>.plusAssign(groups: Collection<Group>) = this.addAll(groups)
 
 
-operator fun GroupAdapter<GroupieViewHolder>.minusAssign(element: Group) = this.remove(element)
+operator fun GroupAdapter<out GroupieViewHolder>.minusAssign(element: Group) = this.remove(element)
 
 
-operator fun GroupAdapter<GroupieViewHolder>.minusAssign(groups: Collection<Group>)  = this.removeAll(groups)
+operator fun GroupAdapter<out GroupieViewHolder>.minusAssign(groups: Collection<Group>)  = this.removeAll(groups)
 
