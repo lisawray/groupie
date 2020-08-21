29feecebca473c25f7bf78b48063107fc23045fe
GarikCarrot <harry-123@yandex.ru>
2020-08-21 07:54:48 +0300

    Revert useless changes

--- library/src/main/java/com/xwray/groupie/Section.java
+++ library/src/main/java/com/xwray/groupie/Section.java
@@ -148,8 +148,8 @@ public class Section extends NestedGroup {
      *
      * This will default detectMoves to true.
      *
-     * @param newBodyGroups The new content of the section
      * @see #update(Collection, boolean)
+     * @param newBodyGroups The new content of the section
      */
     public void update(@NonNull final Collection<? extends Group> newBodyGroups) {
         update(newBodyGroups, true);
